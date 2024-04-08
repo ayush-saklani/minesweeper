9db913dafb4edb2f72574d40f04f8fd4ac5cfd45
“ayush <ayushsaklani04@gmail.com>
2024-04-08 21:48:34 +0530

    heavy improv

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let size = 7;
+let size = 9;
 document.getElementById("go").addEventListener("click", () => {
     let tabparent = document.getElementById("tabparent");
     let existingTable = document.getElementById("tab");
@@ -21,8 +21,55 @@ document.getElementById("go").addEventListener("click", () => {
     }
     tabparent.appendChild(tab); // Add the new table to the parent element
 });
-let flag = 0;
-let a = document.getElementById("11").addEventListener("contextmenu", () => {
-    document.getElementById("11").setAttribute("src", flag % 2 == 0 ? "assets/blank.jpg" : "assets/redflag.jpg");
-    flag++;
+// let flag = 0;
+// let a = document.getElementById("11").addEventListener("contextmenu", () => {
+//     document.getElementById("11").setAttribute("src", flag % 2 == 0 ? "assets/blank.jpg" : "assets/redflag.jpg");
+//     flag++;
+// });
+
+let game = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 9, 0, 0, 0, 0, 9, 0, 0],
+    [0, 0, 9, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 9, 0, 0, 0],
+    [0, 9, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+let gamecal = [
+    [1, 1, 1, 0, 0, 1, 1, 1, 0],
+    [1, 9, 2, 1, 0, 1, 9, 1, 0],
+    [1, 2, 9, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0],
+    [1, 1, 1, 0, 1, 9, 1, 0, 0],
+    [1, 9, 1, 0, 1, 1, 1, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0]
+]
+let calc = ()=>{
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if (game[i][j] != 9) {
+                if ((j - 1) > 0 && game[i][j - 1] == 9) game[i][j]++;
+                if ((j + 1) < size && game[i][j + 1] == 9) game[i][j]++;
+                if ((i - 1) > 0 && game[i - 1][j] == 9) game[i][j]++;
+                if ((i + 1) < size && game[i + 1][j] == 9) game[i][j]++;
+                if ((i - 1) > 0 && (j + 1) < size && game[i - 1][j + 1] == 9) game[i][j]++;
+                if ((i + 1) < size && (j + 1) < size && game[i + 1][j + 1] == 9) game[i][j]++;
+                if ((i - 1) > 0 && (j - 1) > 0 && game[i - 1][j - 1] == 9) game[i][j]++;
+                if ((i + 1) < size && (j - 1) > 0 && game[i + 1][j - 1] == 9) game[i][j]++;
+            }
+        }
+    }
+}
+document.getElementById("show").addEventListener("click", () => {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let img = document.getElementById(i+""+j);
+            img.setAttribute("src", "assets/"+gamecal[i][j]+".jpg");
+        }
+    }
 });
